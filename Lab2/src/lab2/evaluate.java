29b165c8c20ffcc856a8781950f9b2927420ca91
emlwagner <elwagner@udel.edu>
2015-03-05 20:24:50 -0500

    evaluate class with compare methode

--- Lab2/src/lab2/evaluate.java
+++ Lab2/src/lab2/evaluate.java
@@ -84,7 +84,53 @@ public class evaluate {
 		else {
 			return eHandStrength.values()[9];
 			// for right now, it's just returning the eHandStrength value for no pairs, because we aren't yet testing for the other hand strengths
+			}
 		}
+	
+	public int compare(ArrayList<Card> hand1, ArrayList<Card> hand2) {
+		int strength1 = getHandStrength(hand1).getStrength();
+		int strength2 = getHandStrength(hand2).getStrength();
+		if(strength1 == strength2) {
+			if(getHandStrength(hand1) == eHandStrength.values()[1]) {
+				return 0;
+			}
+			else if(getHandStrength(hand1) == eHandStrength.values()[2]) {
+				return 0;
+			}
+			else if(getHandStrength(hand1) == eHandStrength.values()[3]) {
+				Card kicker1 = null;
+				Card kicker2 = null;
+				if(hand1.get(0) == hand1.get(3)) {
+					kicker1 = hand1.get(4);
+				}
+				else if(hand1.get(1) == hand1.get(4)) {
+					kicker1 = hand1.get(0);
+				}
+				if(hand2.get(0) == hand2.get(3)) {
+					kicker2 = hand2.get(4);
+				}
+				else if(hand2.get(1) == hand2.get(4)) {
+					kicker2 = hand2.get(0);
+				}
+				eCardValue v1 = kicker1.getValue();
+				eCardValue v2 = kicker2.getValue();
+				if(v1.getCardValue() > v2.getCardValue()) {
+					return 1;
+				}
+				else if(v2.getCardValue() > v1.getCardValue()) {
+					return 2;
+				}
+			}
 		}
+		else if(strength1 > strength2) {
+			return 1;
+		}
+		else {
+			return 2;
+		}
+		return 0;
+		
+	}
+	
 
 }
